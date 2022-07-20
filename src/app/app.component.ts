0745448de305a545f6f84fdce3f90f5fc0ab353a
Nareg Berberian <naregb94@gmail.com>
2022-07-20 16:00:28 +0200

    added Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  images = ['3.jpg', '1.jpg', '2.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-
-  updateImage() {
-    setInterval (() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000)
-  }
-
-
+export class AppComponent {
 
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss'],
+})
+export class SlideshowComponent implements OnInit {
+
+  images = ['3.jpg', '1.jpg', '2.jpg'];
+  headlines = [
+    'Bring Developing to the Next Level!',
+    'When nobody can, we Start!',
+    'On every Platform!',
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
